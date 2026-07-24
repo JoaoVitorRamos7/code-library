8c6065a0e1df5e8caab355aa30b76e2aaf1a0661
João Vitor Ramos <joaovitorramosdealmeida99@gmail.com>
2026-07-23 21:16:10 -0300

    feat: Adicionar footer
    - Adicionar componente footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="content-container flex justify-center items-center">
+                <p className="text-primary-blue font-bold">Todos os direitos reservados © CodeLibrary</p>
+            </div>
+        </footer>
+    );
+};
+export default Footer;

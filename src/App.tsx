4927cee4c92dc45a2b4b39e6b5c0ae74dc42ccf2
João Vitor Ramos <joaovitorramosdealmeida99@gmail.com>
2026-07-23 23:10:56 -0300

    feat: Adicionar componente filter
    - Adicionar pasta main
    - Adicionar componte filter a pasta main

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/index.jsx";
 import Footer from "./components/Footer/index.js";
+import Main from "./components/Main/index.js";
 function App() {
   return (
     <>
       <Header />
+      <Main />
       <Footer />
     </>
   );
